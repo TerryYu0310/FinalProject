f0fb42444edc5831bb2aad9611bff4fdf92d515a
Terry0310 <u94275@gmail.com>
2021-06-24 23:05:02 +0800

    first commit

---
+++ youtube_player/playlist.js
@@ -0,0 +1,12 @@
+//YouTube Video ID
+let playList = [
+    "Z0n869lCQ0Q",
+    "ACjFPRqmupQ",
+    "2o79_vve6GY",
+    ];
+//播放起訖秒數
+let playTime = [
+    [126,144],
+    [146,172],
+    [154,270],
+];
